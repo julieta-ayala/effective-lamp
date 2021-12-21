6570e39cb898e7ff0222bd71932f19e495c93292
Julieta Ayala <julieta.ayala@tortoise.dev>
2021-12-20 22:35:14 -0600

    add: form prototype with nodejs server and post metoth

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/", function(req,res){
+    res.send("Order placed");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
